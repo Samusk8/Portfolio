67f4cd873d9df5dfb53a11c3d018f207c2e6f31c
Samuel <samueljimenez@paucasesnovescifp.cat>
2026-05-03 23:59:56 +0200

    feat: timeline data

---
+++ portfolio/src/data/timeline.js
@@ -0,0 +1,316 @@
+export const timelineData = [
+  // 🟡 2026
+  {
+    year: 2026,
+    events: [
+      {
+        date: "2026-04-25",
+        position: 1,
+        title: "Campeonato Mallorca",
+        description: "Senior · Élite Libre",
+      },
+      {
+        date: "2026-05-09",
+        position: 1,
+        title: "Campeonato Baleares",
+        description: "Senior · Élite Libre",
+      },
+
+    ]
+  },
+  // 🟡 2025
+  {
+    year: 2025,
+    events: [
+      {
+        date: "2025-05-25",
+        position: 1,
+        title: "Campeonato Baleares",
+        description: "Senior · Élite Libre",
+      },
+      {
+        date: "2025-06-28",
+        position: 1,
+        title: "Campeonato Mallorca",
+        description: "Senior · Élite Libre",
+      },
+      {
+        date: "2025-07-20",
+        position: 13, 
+        title: "Campeonato de España",
+        description: "Senior · Libre",
+      },
+    ]
+  },
+  // 🟡 2024
+  {
+    year: 2024,
+    events: [
+      {
+        date: "2024-07-01",
+        position: 12,
+        title: "Campeonato de España",
+        description: "Junior · Libre",
+      },
+      {
+        date: "2024-06-01",
+        position: 2,
+        title: "Campeonato Baleares",
+        description: "Junior · Élite Libre",
+      },
+      {
+        date: "2024-10-01",
+        position: 1,
+        title: "Trofeo Federación Memorial",
+        description: "Junior · Solo Dance",
+      },
+      {
+        date: "2024-11-01",
+        position: 1,
+        title: "XXVII Trofeo Ciudad de Palma",
+        description: "Junior · Libre",
+      },
+    ],
+  },
+  
+  // 🟡 2023
+  {
+    year: 2023,
+    events: [
+      {
+        date: "2023-06-01",
+        position: 1,
+        title: "Campeonato Baleares",
+        description: "Junior · Élite Libre",
+      },
+      {
+        date: "2023-07-01",
+        position: 12,
+        title: "Campeonato de España",
+        description: "Junior · Libre",
+      },
+      {
+        date: "2023-03-01",
+        position: 1,
+        title: "Campeonato Mallorca",
+        description: "Junior · Parejas Danza",
+      },
+      {
+        date: "2023-05-01",
+        position: 2,
+        title: "Trofeu CPA Sabadell",
+        description: "Junior · Libre",
+      },
+      {
+        date: "2023-10-01",
+        position: 1,
+        title: "Trofeo Federación Memorial",
+        description: "Junior · Parejas Danza",
+      },
+      {
+        date: "2023-11-01",
+        position: 2,
+        title: "Trofeo Alpha",
+        description: "Junior · Élite Libre",
+      },
+      {
+        date: "2023-11-01",
+        position: 1,
+        title: "XXVI Trofeo Ciudad de Palma",
+        description: "Junior · Libre",
+      },
+      {
+        date: "2023-03-01",
+        position: 1,
+        title: "Trofeo Nacional Arenys de Munt",
+        description: "Junior · Libre",
+      },
+    ],
+  },
+
+  // 🟡 2022
+  {
+    year: 2022,
+    events: [
+      {
+        date: "2022-06-05",
+        position: 1,
+        title: "Campeonato Baleares",
+        description: "Juvenil · Élite Libre",
+      },
+      {
+        date: "2022-07-01",
+        position: 1,
+        title: "Campeonato Mallorca",
+        description: "Juvenil · Élite Libre",
+      },
+      {
+        date: "2022-07-01",
+        position: 10,
+        title: "Campeonato de España",
+        description: "Juvenil · Libre",
+      },
+      {
+        date: "2022-03-06",
+        position: 1,
+        title: "Campeonato Baleares",
+        description: "Cuartetos",
+      },
+      {
+        date: "2022-01-01",
+        position: null,
+        title: "Campeonato de España",
+        description: "Grupos Show",
+      },
+      {
+        date: "2022-05-01",
+        position: 1,
+        title: "Trofeo Nacional Arenys de Munt",
+        description: "Juvenil · Libre",
+      },
+      {
+        date: "2022-11-05",
+        position: 1,
+        title: "XXVTrofeo Ciudad de Palma",
+        description: "Juvenil · Libre",
+      },
+    ],
+  },
+
+  // 🟡 2021
+  {
+    year: 2021,
+    events: [
+      {
+        date: "2021-06-20",
+        position: 1,
+        title: "Campeonato Baleares",
+        description: "Cadete · Élite Libre",
+      },
+      {
+        date: "2021-08-11",
+        position: 1,
+        title: "Campeonato Mallorca",
+        description: "Cadete · Élite Libre",
+      },
+      {
+        date: "2021-08-11",
+        position: 1,
+        title: "Campeonato Mallorca",
+        description: "Grupos Show · Pequeño",
+      },
+      {
+        date: "2021-07-01",
+        position: 18,
+        title: "Campeonato de España",
+        description: "Cadete · Libre",
+      },
+      {
+        date: "2021-11-07",
+        position: 1,
+        title: "XXIV Trofeo Ciudad de Palma",
+        description: "Cadete · Libre",
+      },
+      {
+        date: "2021-11-01",
+        position: 3,
+        title: "Trofeo Alpha",
+        description: "Cadete · Libre",
+      },
+      {
+        date: "2021-05-07",
+        position: null,
+        title: "V Trofeo Ciudad de Alcoy",
+        description: "Grupos Show · Pequeño",
+      },
+      {
+        date: "2021-05-07",
+        position: null,
+        title: "Copa de España Ciudad de Alcoy",
+        description: "Grupos Show · Junior",
+      },
+      {
+        date: "2021-01-01",
+        position: null,
+        title: "X Campeonato de España",
+        description: "Cuartetos",
+      },
+    ],
+  },
+
+  // 🟡 2020
+  {
+    year: 2020,
+    events: [
+      {
+        date: "2020-06-01",
+        position: 1,
+        title: "Campeonato Baleares",
+        description: "Cadete · Élite B",
+      },
+      {
+        date: "2020-06-01",
+        position: 1,
+        title: "Campeonato Baleares FFOO",
+        description: "Cadete · Élite B · Figuras Obligatorias",
+      },
+      {
+        date: "2020-06-01",
+        position: 1,
+        title: "Campeonato Mallorca",
+        description: "Cadete · Absoluto",
+      },
+      {
+        date: "2020-11-08",
+        position: 1,
+        title: "XXIII Trofeo Ciudad de Palma",
+        description: "Cadete · Élite B",
+      },
+    ],
+  },
+
+  // 🟡 2019
+  {
+    year: 2019,
+    events: [
+      {
+        date: "2019-11-23",
+        position: 3,
+        title: "Trofeo El Foix",
+        description: "Iniciación B",
+      },
+      {
+        date: "2019-06-01",
+        position: 1,
+        title: "Trofeo La Salle",
+        description: "Iniciación B",
+      },
+    ],
+  },
+
+  // 🟡 2018
+  {
+    year: 2018,
+    events: [
+      {
+        date: "2018-10-01",
+        position: 2,
+        title: "XXI Trofeo Ciudad de Palma",
+        description: "Iniciación B",
+      },
+    ],
+  },
+
+  // 🟡 2017
+  {
+    year: 2017,
+    events: [
+      {
+        date: "2017-11-18",
+        position: 1,
+        title: "Trofeo El Foix",
+        description: "Iniciación B",
+      },
+    ],
+  },
+];
